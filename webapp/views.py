0ad918440fed3746347f73653d1690f232188ce8
Chandra Prakash <prakash_c@worksap.co.jp>
2018-03-02 19:25:06 +0800

    add sample git in python

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from subprocess import Popen, PIPE
+from os import path
 
 def index(request) :
-    return HttpResponse("<h2>Hola!</h2>")
+    git_command = ['/usr/bin/git', 'status']
+    repository = path.dirname('/home/cp/Desktop/code/git/django-tut/learn-django/')
+    git_query = Popen(git_command, cwd=repository, stdout=PIPE, stderr=PIPE)
+    (git_status, error) = git_query.communicate()
+    if git_query.poll() == 0:
+        print(git_status)
+
+    return HttpResponse("<h2>Hola!</h2>"+str(git_status, 'utf-8'))
